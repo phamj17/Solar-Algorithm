dd5f544e3c46f910cb9a077b4bb9cb1dc5fc4ad8
lumpster24 <binghami17@up.edu>
2018-02-06 14:26:14 -0800

    Can obtain gps output. Long and lat directions implemented.

--- gps/gps.ino
+++ gps/gps.ino
@@ -1,63 +1,23 @@
-// SoftwareSerial - Version: Latest 
 #include <SoftwareSerial.h>
-//#include <Gpsneo.h>
+//#include "spa.h"
+//#include "Buffers.h"
+//#include "SCI0.h"
 
-/*
-  Serial Event example
-
- When new serial data arrives, this sketch adds it to a String.
- When a newline is received, the loop prints the string and
- clears it.
-
- A good test for this is to try it with a GPS receiver
- that sends out NMEA 0183 sentences.
-
- Created 9 May 2011
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/SerialEvent
-
- */
 SoftwareSerial mySerial(10,11); //RX, TX
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
 String timee, latitude, longitude, date;
+int lat_dir, long_dir;
 boolean flag = false;
 
 void setup() {
   // initialize serial:
   Serial.begin(9600);
   mySerial.begin(9600);
-  // reserve 200 bytes for the inputString:
-  inputString.reserve(200);
+  inputString.reserve(200); // reserve 200 bytes for the inputString:
 }
 
 void loop() {
-  // print the string when a newline arrives:
-  /*
-  if (stringComplete) {
-    Serial.print("\nTime: ");
-    Serial.print(timee);
-    Serial.print("\nLatitude: ");
-    Serial.print(latitude);
-    Serial.print("\nLongitude: ");
-    Serial.print(longitude);
-    Serial.print("\nDate: ");
-    Serial.print(date);
-    Serial.print("\n");
-    Serial.print(inputString);
-    // clear the string:
-    inputString = "";
-    timee = "";
-    latitude = "";
-    longitude = "";
-    date = "";
-    stringComplete = false;
-  }
-  */
-  
   if(mySerial.available()){
     //Serial.write(mySerial.read());
     char inChar = (char)mySerial.read();
@@ -72,7 +32,6 @@ void loop() {
     inputString = "";
     stringComplete = false;
   }
-  //parse();
 }
 
 void parse(String in)
@@ -106,12 +65,32 @@ void parse(String in)
         latitude = val;
         Serial.println("latitude: " + latitude);
         break;
-      //case 4: //latitude direction
+      case 4: //latitude direction
+        if (val.equals("N"))
+        {
+          lat_dir = 1;
+        }
+        else
+        {
+          lat_dir = -1;
+        }
+        Serial.println("lat_dir: " + String(lat_dir));
+        break;
       case 5: //longitude
         longitude = val;
         Serial.println("longitude: " + longitude);
         break;
-      //case 6: //longitude direction
+      case 6: //longitude direction
+        if (val.equals("E"))
+        {
+          long_dir = 1;
+        }
+        else
+        {
+          long_dir = -1;
+        }
+        Serial.println("long_dir: " + String(long_dir));
+        break;
       case 9: //date
         date = val;
         Serial.println("date: " + date);
@@ -126,61 +105,9 @@ void parse(String in)
   secondComma = 0;
   return;
 }
-
 /*
-void parse() {
-  while (mySerial.available()) {
-    // get the new byte:
-    Serial.println("reading data...\n");
-    char inChar = (char)mySerial.read();
-    // add it to the inputString:
-    inputString += inChar;
-    //Serial.println(inputString);
-    // if the incoming character is a newline, set a flag
-    // so the main loop can do something about it:
-    //char * GPRMC = "$GPRMC, ";
-    //char * temp_input = inputString;
-    if (inputString.equals("$GPRMC, ")==0) {
-      flag = true;
-      //Serial.print(flag);
-    }
-
-    if (inChar == '\n' && flag) {
-      Serial.println("wudddddduuuuuppppppp");
-      // 1: $GPRMC
-      // 2: UTC Position: hhmmss.sss
-      // 4: Latitude: ddmm.mmmm
-      // 6: Longitude: ddmm.mmmm
-      // 10: Date: ddmmyy
-      char * start = &inputString[0];
-      char * temp = strtok(start, ",");
-      int count = 1;
-      while (temp != NULL) {
-       temp = strtok(NULL, ",\n");
-       count++;
-       switch (count) {
-        case 2:
-          timee = temp;
-          Serial.println("time: " + timee);
-          break;
-        case 4:
-          latitude = temp;
-          Serial.println("latitude: " + latitude);
-          break;
-        case 6:
-          longitude = temp;
-          Serial.println("longitude: " + longitude);
-          break;
-        case 10:
-          date = temp;
-          Serial.println("date: " + date);
-          break;
-       }
-      }
-      Serial.println("done with while.....................................");
-      stringComplete = true;
-    }
-  }
+void track()
+{
+  
 }
 */
-
