b2dac30472a09fdeb14dd83671495129242b4b2f
Justin Pham <phamj17@up.edu>
2018-02-20 21:03:59 -0800

    SPA inputs from GPS are converting correctly

--- entry/entry.ino
+++ entry/entry.ino
@@ -1,8 +1,8 @@
 #include <SoftwareSerial.h>
-#include "P:\seniorProject\Solar-Algorithm\spa.h"
+//#include "P:\seniorProject\Solar-Algorithm\spa.h"
+#include "C:\Users\Pham\Documents\Arduino\Solar-Algorithm\spa.h"
 //#include "P:\seniorProject\Solar-Algorithm\Buffers.h"
 //#include "P:\seniorProject\Solar-Algorithm\SCI0.h"
-#include <LiquidCrystal.h>
 
 #define LATITUDE 0
 #define LONGITUDE 1
@@ -14,11 +14,6 @@ String timee, latitude, longitude, date;
 int lat_dir, long_dir;
 boolean flag = false;
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
-const int rs = 50, en = 52, d4 = 47, d5 = 49, d6 = 51, d7 = 53;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
 spa_data spa;
 
 void setup() {
@@ -26,9 +21,6 @@ void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
   inputString.reserve(200); // reserve 200 bytes for the inputString:
-
-  // set up the LCD's number of columns and rows:
-  lcd.begin(20, 4);
 }
 
 void loop() {
@@ -41,13 +33,11 @@ void loop() {
   }
 
   if(stringComplete){
-    Serial.print(inputString);
+    //Serial.print(inputString);
     parse(inputString);
     inputString = "";
     stringComplete = false;
-
     refresh_spa_data();
-    lcdprint();
   }
 }
 
@@ -117,16 +107,18 @@ void refresh_spa_data(void)
 {
   spa.timezone = -8.0; //double
   //int
-  spa.year = ("20" + date.substring(4,5)).toInt();
-  spa.month = (date.substring(0,1)).toInt();
-  spa.day = (date.substring(2,3)).toInt();  
-  spa.hour = timee.substring(0,1).toInt() + spa.timezone;
-  spa.minute = timee.substring(2,3).toInt();
-  spa.second = timee.substring(4,5).toInt();
+  spa.year = ("20" + date.substring(4,6)).toInt();
+  spa.month = (date.substring(2,4)).toInt();
+  spa.day = (date.substring(0,2)).toInt();
+  spa.hour = timee.substring(0,2).toInt() + spa.timezone;
+  if (spa.hour < 0)
+    spa.hour += 24;
+  spa.minute = timee.substring(2,4).toInt();
+  spa.second = timee.substring(4,6).toInt();
   //double
   //spa.delta_t = wtf
-  spa.longitude = convert_coordinates(LONGITUDE);
-  spa.latitude = convert_coordinates(LATITUDE);
+  spa.longitude = convert_coordinates(1);
+  spa.latitude = convert_coordinates(0);
   /*
   spa.elevation =
   spa.pressure =
@@ -136,6 +128,12 @@ void refresh_spa_data(void)
   spa.atmos_refrac =
   spa.function = //int
   */
+  char buf[200];
+  sprintf(buf, "year: %d\nmonth: %d\nday: %d\nhour: %d\nminute: %d\nsecond: %d\n", spa.year,spa.month,spa.day,spa.hour,spa.minute,spa.second);
+  Serial.println("Longitude: " + String(spa.longitude));
+  Serial.println("Latitude: " + String(spa.latitude));
+  Serial.println("Timezone: " + String(spa.timezone));
+  Serial.println(buf);
 }
 
 
@@ -152,32 +150,15 @@ double convert_coordinates(int code)
   {
     period = latitude.indexOf('.', 0);
     if (period)
-      return latitude.toInt() / 100.00 * lat_dir;
+      return latitude.toInt() / 100.00000 * lat_dir;
   }
   else
   {
     period = longitude.indexOf('.', 0);
     if (period)
-      return longitude.toInt() / 100.00 * long_dir;
+      return longitude.toInt() / 100.00000 * long_dir;
   }
   return 0;
-}
-
-
-void lcdprint()
-{
-  //print to lcd
-  lcd.setCursor(0, 0);
-  lcd.print("time: %d:%d:%d", spa.hour, spa.minute, spa.second);
-  
-  lcd.setCursor(0, 1);
-  lcd.print("lat: %lf", spa.latitude);
-  
-  lcd.setCursor(0, 2);
-  lcd.print("long: %lf", spa.longitude);
-  
-  lcd.setCursor(0, 3);
-  lcd.print("date: %d/%d/%d", spa.month, spa.day, spa.year);
 }
 
 
